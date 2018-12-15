a9a88408598e4bd0238d1d272027c837c7d3087d
shadibdair <43513994+shadibdair@users.noreply.github.com>
2018-12-15 10:53:01 +0200

    update

--- ejs-js.upload-img/app.js
+++ ejs-js.upload-img/app.js
@@ -23,7 +23,7 @@ const upload = multer({
 // Check File Type
 function checkFileType(file, cb){
   // Allowed ext
-  const filetypes = /jpeg|jpg|png|gif/;
+  const filetypes = /jpeg|jpg|png|gif|txt/;
   // Check ext
   const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
   // Check mime
